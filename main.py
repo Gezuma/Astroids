a50557719916f854b697ed80fadd8ab8ef1d1027
Gezuma <Gezuma@ .com>
2025-01-07 21:47:30 +0100

    Version B - added a game loop with an updating display and added the option to close the window on the X button of the Window

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ import pygame
 import constants
 
 def main():
+    # initializing all pygame modules that can be initialized without arguments etc.
+    pygame.init()
     print("Starting asteroids!")
+    
+    # sets the display size for the game
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    # Game loop
+    while True:
+        # this should be at the beginning of the game loop, so the game can be closed by pressing the close button "X" on the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # stuff loop that need to loop
+
+        # display.flip() should always be at the end to refresh the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
