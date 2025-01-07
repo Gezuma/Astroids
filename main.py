0297321b8accd34765a82391e115fc2cf1cba161
Gezuma <Gezuma@ .com>
2025-01-07 21:20:41 +0100

    Version A - creating READme, main.py, constants.py, requirements.txt

---
+++ main.py
@@ -0,0 +1,15 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+# this is where constants "variables" are assigned. Constants like SCREENWIDTH, ASTRIOD_MIN_RADIOS and ASTROID_SPAWN_RATE etc.
+import constants
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+if __name__ == "__main__":
+    main()
