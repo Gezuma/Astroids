2301631088bdbe69984c162e9ed23d854ff84bff
Gezuma <Gezuma@ .com>
2025-01-08 16:55:06 +0100

    Version D - added player.py and draws ships

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ import pygame
 # this is where constants "variables" are assigned. Constants like SCREENWIDTH, ASTRIOD_MIN_RADIOS and ASTROID_SPAWN_RATE etc.
 import constants
 
+import player
+
 def main():
     # initializing all pygame modules that can be initialized without arguments etc.
     pygame.init()
     clock_speed = pygame.time.Clock()  # object created to later set FPS
     dt = 0
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
     
     # sets the display size for the game
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    player_obj = player.Player(x,y)
+
 
     # Game loop
     while True:
@@ -25,10 +32,22 @@ def main():
         # will be added later: 
         #  more checks for player inputs
         #  update the game world
+        # will be added later: 
+        #  more checks for player inputs
+        #  update the game world
 
         # display.flip() should always be at the end to refresh the screen
         screen.fill("black")
+ 
+
+        # refresh player object
+        player_obj.draw(screen)
+
+
         pygame.display.flip()
+
+        # limits framerate to 60 fps and set dt to amount of time since last it was called in seconds
+        dt=clock_speed.tick(60)/1000 
 
         # limits framerate to 60 fps and set dt to amount of time since last it was called in seconds
         dt=clock_speed.tick(60)/1000 
