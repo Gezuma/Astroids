46f5f93df6126142e004e144b3835f32f8905942
Gezuma <Gezuma@ .com>
2025-01-08 09:16:08 +0100

    Version C - added 60 fps control and prepared delta time to later balance speed of objects to main.py, added new file circleshape.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ import constants
 def main():
     # initializing all pygame modules that can be initialized without arguments etc.
     pygame.init()
-    print("Starting asteroids!")
+    clock_speed = pygame.time.Clock()  # object created to later set FPS
+    dt = 0
     
     # sets the display size for the game
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    print("Screen width: 1280")
-    print("Screen height: 720")
 
     # Game loop
     while True:
@@ -23,10 +22,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        # stuff loop that need to loop
+        # will be added later: 
+        #  more checks for player inputs
+        #  update the game world
 
         # display.flip() should always be at the end to refresh the screen
+        screen.fill("black")
         pygame.display.flip()
+
+        # limits framerate to 60 fps and set dt to amount of time since last it was called in seconds
+        dt=clock_speed.tick(60)/1000 
 
 if __name__ == "__main__":
     main()
